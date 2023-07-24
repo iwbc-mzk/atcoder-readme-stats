8c7804d551ca7899106a8789c1c9d9cb2eefb37a
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-24 19:24:29 +0900

    refactoring stats card

--- src/cards/stats.py
+++ src/cards/stats.py
@@ -3,6 +3,7 @@ import html
 
 from pydantic import BaseModel
 
+from src.cards.card import Card
 from src.atcoder import UserData
 from src.utils import get_rating_color
 from src.themes import Theme, THEMES
@@ -36,10 +37,37 @@ KEY_LABEL_MAP = {
 }
 
 
-class StatsCard:
+class StatsCard(Card):
     def __init__(self, userdata: UserData, option: StatsOption = StatsOption()) -> None:
         self._userdata = userdata
         self._option = option
+
+        self._prepare()
+
+        super().__init__(
+            width=self._option.width,
+            height=self._option.height,
+            viewbox_height=self._viewbox_height,
+            theme=self._option.theme,
+        )
+
+    def _prepare(self) -> None:
+        self._set_history_row_num()
+        self._set_viewbox_height()
+
+    def _set_history_row_num(self):
+        compe_history_num = len(self._userdata.competitions_history)
+        self._history_row_num = (
+            min(self._option.show_history, compe_history_num)
+            if type(self._option.show_history) == int
+            else min(3, compe_history_num)
+        )
+
+    def _set_viewbox_height(self) -> None:
+        # Need to execute _set_history_row_num first.
+        self._viewbox_height = 200
+        if self._option.height == "auto" and self._option.show_history:
+            self._viewbox_height += 36 * (self._history_row_num + 1) + 15
 
     def _field_to_label(self, field: str) -> str:
         return KEY_LABEL_MAP[field]
@@ -75,7 +103,158 @@ class StatsCard:
                 """
             for i, stat in enumerate(stats)
         ]
-        style = f"""
+
+        return f"""
+            <table style="width: 100%">
+                {"".join(stats_rows)}
+            </table>
+        """
+
+    def _renderRatingCircle(self, rating: int) -> str:
+        return f"""
+            <div class="container fadein">
+                <div class="circle">
+                    <div class="rating">
+                        <span class="rating-label">Rating</span>
+                        <br />
+                        <span class="rating-text">{rating}</span>
+                    </div>
+                </div>
+            </div>
+        """
+
+    def _render_competitions_history(self):
+        competitions = sorted(
+            self._userdata.competitions_history, key=lambda x: x.date, reverse=True
+        )
+
+        competitions_rows = []
+        for i, compe in enumerate(competitions):
+            if i >= self._history_row_num:
+                break
+
+            competitions_rows.append(
+                f"""
+                <tr class="fadein compe-row" style="animation-delay: {(i + 8) * 150}ms">
+                    <td class="compe-val val-date">{compe.date.strftime("%Y-%m-%d")}</td>
+                    <td class="compe-val"><div class="val-contest">{html.escape(compe.contest)}</div></td>
+                    <td class="compe-val">{compe.rank}</td>
+                    <td 
+                        class="compe-val" 
+                        {f'style="color: {get_rating_color(compe.performance)}"' if type(compe.performance) == int else ""}
+                    >
+                            {compe.performance if compe.performance else "-"}
+                    </td>
+                </tr>
+            """
+            )
+
+        return f"""
+            <table class="compe-table">
+                <colgroup>
+                    <col width="20%" />
+                    <col width="50%" />
+                    <col width="15%" />
+                    <col width="15%" />
+                </colgroup>
+                <tr class="fadein" style="animation-delay: {7 * 150}ms">
+                    <th>Date</th>
+                    <th>Contest</th>
+                    <th>Rank</th>
+                    <th>Perf</th>
+                </tr>
+                {"".join(competitions_rows)}
+            </table>
+        """
+
+    def _render_title(self) -> str:
+        return f"""
+            <div id="title" class="fadein">{self._userdata.id}'s Atcoder Stats</div>
+        """
+
+    def _render_body(self):
+        show_history = bool(self._option.show_history)
+
+        return f"""
+            <div id="stats-body">
+                <div id="stats">
+                    {self._renderStats()}
+                </div>
+                <div id="rank-circle">
+                    {self._renderRatingCircle(self._userdata.rating)}
+                </div>
+            </div>
+            {f'''
+                <div class="border"></div>
+                <div id="competitions-history-body">
+                    {self._render_competitions_history()}
+                </div>''' if show_history else ""}
+        """
+
+    def _styles(self) -> str:
+        theme = self._option.theme
+
+        color = get_rating_color(self._userdata.rating)
+        deg = 360 * (self._userdata.rating % 400) // 400
+
+        # 暫定対応
+        # できれば@propertyやanimationを使いたい
+        keyframes = []
+        N = 30
+        for i in range(N + 1):
+            prop = f"""
+                {i * 100 // N}% {{
+                    background-image: radial-gradient({self._option.theme.background_color} 60%, transparent 61%),
+                    conic-gradient({color} {i * deg/N}deg, {color}33 {i * deg/N}deg 360deg);
+                }}
+            """
+            keyframes.append(prop)
+
+        return f"""
+            #title {{
+                color: {theme.title_color};
+                font-size: 20px;
+                font-weight: 600;
+                margin-bottom: 10px;
+            }}
+            #stats-body {{
+                display: flex;
+                flex-direction: row;
+            }}
+            #stats {{
+                width: 60%;
+            }}
+            #rank-circle {{
+                width: 40%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }}
+            #competitions-history-body {{
+                width: 100%;
+            }}
+            .border {{
+                height: 2px;
+                background-color: rgb(228, 226, 226);
+                margin: 5px 20px 0px;
+            }}
+            .fadein {{
+                opacity: 0;
+                animation-name: fadein;
+                animation-duration: 0.8s;
+                animation-timing-function: ease-in-out;
+                animation-fill-mode: forwards;
+            }}
+            @keyframes fadein {{
+                0% {{
+                    opacity: 0;
+                }}
+                100% {{
+                    opacity: 1;
+                }}
+            }}
+
+            /* Stats */
             .stats-row {{
                 height: 28px;
                 display: flex;
@@ -96,33 +275,8 @@ class StatsCard:
                 margin: auto 5px auto 0px;
                 color: {self._option.theme.icon_color};
             }}
-        """
 
-        return f"""
-            <table style="width: 100%">
-                {"".join(stats_rows)}
-            </table>
-            <style id="stats-style">{style}</style>
-        """
-
-    def _renderRatingCircle(self, rating: int) -> str:
-        color = get_rating_color(rating)
-        deg = 360 * (rating % 400) // 400
-
-        # 暫定対応
-        # できれば@propertyやanimationを使いたい
-        keyframes = []
-        N = 30
-        for i in range(N + 1):
-            prop = f"""
-                {i * 100 // N}% {{
-                    background-image: radial-gradient({self._option.theme.background_color} 60%, transparent 61%),
-                    conic-gradient({color} {i * deg/N}deg, {color}33 {i * deg/N}deg 360deg);
-                }}
-            """
-            keyframes.append(prop)
-
-        style = f"""
+            /* Rating Circle */
             /*
             これを使ってランクサークルのアニメーションができそうだが上手くいかない
             @property --deg {{
@@ -175,47 +329,8 @@ class StatsCard:
             @keyframes conic-gradient {{
                 {"".join(keyframes)}
             }}
-        """
-        return f"""
-            <div class="container fadein">
-                <div class="circle">
-                    <div class="rating">
-                        <span class="rating-label">Rating</span>
-                        <br />
-                        <span class="rating-text">{rating}</span>
-                    </div>
-                </div>
-            </div>
-            <style id="rating-circle-style">{style}</style>
-        """
 
-    def _render_competitions_history(self, row_num):
-        competitions = sorted(
-            self._userdata.competitions_history, key=lambda x: x.date, reverse=True
-        )
-
-        competitions_rows = []
-        for i, compe in enumerate(competitions):
-            if i >= row_num:
-                break
-
-            competitions_rows.append(
-                f"""
-                <tr class="fadein compe-row" style="animation-delay: {(i + 8) * 150}ms">
-                    <td class="compe-val val-date">{compe.date.strftime("%Y-%m-%d")}</td>
-                    <td class="compe-val"><div class="val-contest">{html.escape(compe.contest)}</div></td>
-                    <td class="compe-val">{compe.rank}</td>
-                    <td 
-                        class="compe-val" 
-                        {f'style="color: {get_rating_color(compe.performance)}"' if type(compe.performance) == int else ""}
-                    >
-                            {compe.performance if compe.performance else "-"}
-                    </td>
-                </tr>
-            """
-            )
-
-        style = f"""
+            /* Competition History */
             .compe-val {{
                 font-size: 14px;
                 font-weight: 700;
@@ -245,142 +360,4 @@ class StatsCard:
                 -webkit-box-orient: vertical;
                 -webkit-line-clamp: 2;
             }}
-        """
-
-        return f"""
-            <table class="compe-table">
-                <colgroup>
-                    <col width="20%" />
-                    <col width="50%" />
-                    <col width="15%" />
-                    <col width="15%" />
-                </colgroup>
-                <tr class="fadein" style="animation-delay: {7 * 150}ms">
-                    <th>Date</th>
-                    <th>Contest</th>
-                    <th>Rank</th>
-                    <th>Perf</th>
-                </tr>
-                {"".join(competitions_rows)}
-            </table>
-            <style>{style}</style>
-        """
-
-    def render(self):
-        width = self._option.width
-        height = self._option.height
-        theme = self._option.theme
-        show_history = bool(self._option.show_history)
-
-        compe_history_num = len(self._userdata.competitions_history)
-        history_row_num = (
-            min(self._option.show_history, compe_history_num)
-            if type(self._option.show_history) == int
-            else min(3, compe_history_num)
-        )
-
-        viewbox_height = 200
-        if height == "auto" and show_history:
-            viewbox_height += 36 * (history_row_num + 1) + 15
-
-        style = f"""
-            #svg-body {{
-                margin: 0;
-                font-family: {theme.font_family};
-                color: {theme.text_color};
-                height: 100%;
-                width: 100%;
-            }}
-            #card {{
-                width: calc(100% - 2px);
-                height: calc(100% - 2px);
-                
-                display: flex;
-                position: relative;
-                background-color: {theme.background_color};
-
-                border: 1px solid rgb(228, 226, 226);
-                border-radius: 10px;
-            }}
-            #card-body {{
-                margin: 20px;
-                width: calc(100% - 40px);
-                height: calc(100% - 40px);
-                display: flex;
-                flex-direction: column;
-            }}
-            #title {{
-                color: {theme.title_color};
-                font-size: 20px;
-                font-weight: 600;
-                margin-bottom: 10px;
-            }}
-            #stats-body {{
-                display: flex;
-                flex-direction: row;
-            }}
-            #stats {{
-                width: 60%;
-            }}
-            #rank-circle {{
-                width: 40%;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-            }}
-            #competitions-history-body {{
-                width: 100%;
-            }}
-            .border {{
-                height: 2px;
-                background-color: rgb(228, 226, 226);
-                margin: 5px 20px 0px;
-            }}
-            .fadein {{
-                opacity: 0;
-                animation-name: fadein;
-                animation-duration: 0.8s;
-                animation-timing-function: ease-in-out;
-                animation-fill-mode: forwards;
-            }}
-            @keyframes fadein {{
-                0% {{
-                    opacity: 0;
-                }}
-                100% {{
-                    opacity: 1;
-                }}
-            }}
-        """
-        return f"""
-            <svg version="1.1" 
-                viewBox="0 0 450 {viewbox_height}"
-                xmlns="http://www.w3.org/2000/svg"
-                {f'width="{width}"' if type(width) == int else ""}
-                {f'height="{height}"' if type(height) == int else ""}
-            >
-                <foreignObject width="450" height="{viewbox_height}" requiredExtensions="http://www.w3.org/1999/xhtml">
-                    <body id="svg-body" xmlns="http://www.w3.org/1999/xhtml">
-                        <div id="card">
-                            <div id="card-body">
-                                <div id="title" class="fadein">{self._userdata.id}'s Atcoder Stats</div>
-                                <div id="stats-body">
-                                    <div id="stats">
-                                        {self._renderStats()}
-                                    </div>
-                                    <div id="rank-circle">
-                                        {self._renderRatingCircle(self._userdata.rating)}
-                                    </div>
-                                </div>
-                                {f'''
-                                    <div class="border"></div>
-                                    <div id="competitions-history-body">
-                                        {self._render_competitions_history(history_row_num)}
-                                    </div>''' if show_history else ""}
-                            </div>
-                        </div>
-                        <style id="main-style">{style}</style>
-                    </body>
-                </foreignObject>
-            </svg>
         """
