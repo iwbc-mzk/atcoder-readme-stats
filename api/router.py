6482392dca99af17326c96d5743b5f22edb59b23
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-29 14:23:50 +0900

    add heatmap endpoint

--- api/router.py
+++ api/router.py
@@ -5,8 +5,10 @@ from fastapi import FastAPI
 from fastapi.responses import Response
 
 from src.cards.stats import StatsCard, StatsOption
+from src.cards.heatmap import HeatmapCard, HeatmapOption
 from src.cards.error import ErrorCard
 from src.atcoder import Atcoder as atcoder
+from src.atcoder_problems import AtcoderProblems as ap
 from src.themes import THEMES
 from src.const import ONE_DAY_SECOND
 
@@ -53,6 +55,46 @@ async def stats(
         return Response(content=svg, headers=headers, media_type="image/svg+xml")
 
     card = StatsCard(userdata, option)
+    svg = io.BytesIO(bytes(card.render(), "utf-8")).getvalue()
+
+    headers = {
+        "Cache-Control": f"max-age={ONE_DAY_SECOND // 2},stale-while-revalidate={ONE_DAY_SECOND},s-maxage={ONE_DAY_SECOND}",
+    }
+
+    return Response(content=svg, headers=headers, media_type="image/svg+xml")
+
+
+@app.get("/heatmap/{username}")
+async def stats(
+    # path parameter
+    username: str,
+    # query parameter
+    width: Optional[Union[int, Auto]] = None,
+    height: Optional[Union[int, Auto]] = None,
+    hide: Optional[str] = None,  # ex: hide=rating,last_competed
+    theme: Optional[str] = None,
+):
+    option = HeatmapOption()
+    if width:
+        option.width = width
+    if height:
+        option.height = height
+    if hide:
+        option.hide = set(hide.split(","))
+    if theme:
+        option.theme = THEMES[theme] if theme in THEMES else THEMES["default"]
+
+    try:
+        submissions = ap.fetch_submissions(username)
+    except ValueError as e:
+        card = ErrorCard(e.args[0], e.args[1])
+        svg = io.BytesIO(bytes(card.render(), "utf-8")).getvalue()
+        headers = {
+            "Cache-Control": "no-cache, no-store, must-revalidate",
+        }
+        return Response(content=svg, headers=headers, media_type="image/svg+xml")
+
+    card = HeatmapCard(username, submissions, option)
     svg = io.BytesIO(bytes(card.render(), "utf-8")).getvalue()
 
     headers = {
